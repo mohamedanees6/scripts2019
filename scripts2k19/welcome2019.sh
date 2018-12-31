88491f1e9e3a2597a4dbcae66cb67aed1747b967
Mohamed Anees <anees.m@practo.com>
2019-01-01 01:34:09 +0530

    My Mac Init Command

--- scripts2k19/welcome2019.sh
+++ scripts2k19/welcome2019.sh
@@ -1,1 +1,20 @@
-echo "Welcome 2019!"
+clear
+echo "========================================="
+echo "Welcome Anees!"
+
+echo "We are booting your Mac with super tools! Please wait"
+
+echo "========================================="
+brew services start redis
+pg_ctl start
+open -a eclipse
+open -a open -a Visual\ Studio\ Code 
+open -a popsql
+open -a google\ chrome
+open -a sublime\ text
+open -a google\ chrome https://mail.google.com
+open -a google\ chrome https://stackoverflow.com
+open -a google\ chrome https://dzone.com
+open -a slack
+
+echo "All init applications started!"

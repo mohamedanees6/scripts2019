e9d92f9114eb4ebe1ced847fbd827d93b6bc4a4c
Mohamed Anees <anees.m@practo.com>
2019-02-22 15:29:00 +0530

    Adding nested installation steps

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:mohamedanees6/scripts2019.git
-cd scripts2019/scripts2k19/git_scripts && cat fuckthepull.sh >> ~/.bash_profile
-source ~/.bash_profile
+git clone git@github.com:mohamedanees6/scripts2019.git &&
+cd scripts2019/scripts2k19/git_scripts && cat fuckthepull.sh >> ~/.bash_profile &&
+source ~/.bash_profile &&
 echo fuckthepull- Installed successfully.
